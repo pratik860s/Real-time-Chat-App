482dee4c193a170c0e004dd7e82e6fb5208c00a9
pratik860s <pratiksharma860@gmail.com>
2024-03-17 01:22:24 +0530

    first

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+const router = express.Router()
+
+const {registerUser,authUser,allUsers}=require("../controllers/userController")
+const { protect} = require("../middleware/authMiddleware")
+
+router.post('/',registerUser)
+router.get("/",protect,allUsers)
+router.post('/login',authUser)
+
+module.exports=router
